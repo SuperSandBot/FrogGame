59c0d69b594349756ab72afddafa7240462b4fe4
DELL <minhcanh512@gmail.com>
2021-10-26 01:18:05 +0700

    backgroundMusic

--- app/src/main/java/com/example/froggame/MainActivity.java
+++ app/src/main/java/com/example/froggame/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.froggame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaParser;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    boolean soundCheck = false;
+     private MediaPlayer backgroundMusic;
+     boolean soundCheck = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,16 +31,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intentSetting);
             }
         });
+        backgroundMusic=MediaPlayer.create(this,R.raw.nhac);
+        backgroundMusic.setLooping(true);
         btnSound.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (soundCheck)
                 {
                     btnSound.setBackground(getDrawable(R.drawable.sound));
+                    backgroundMusic.setLooping(true);
+                    backgroundMusic.start();
                     soundCheck = false;
                 }
                 else
                 {
+                    backgroundMusic.pause();
                     btnSound.setBackground(getDrawable(R.drawable.sound_off));
                     soundCheck = true;
                 }
@@ -51,5 +58,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!backgroundMusic.isPlaying()){
+            backgroundMusic.start();
+        }
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        backgroundMusic.pause();
     }
 }
